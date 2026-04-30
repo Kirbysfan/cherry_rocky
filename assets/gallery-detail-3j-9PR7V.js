c5421f66d85230c677899d1b854c7f96898ab9b2
Kirbysfan <108968369+Kirbysfan@users.noreply.github.com>
2026-04-30 15:40:36 +0800

    push

--- assets/gallery-detail-3j-9PR7V.js
+++ assets/gallery-detail-3j-9PR7V.js
@@ -1,4 +1,4 @@
-import { _ as _export_sfc, c as createElementBlock, j as withDirectives, n as normalizeClass, b as createBaseVNode, t as toDisplayString, a as createVNode, w as withCtx, d as createTextVNode, i as createCommentVNode, k as resolveDirective, r as resolveComponent, u as useRouter, o as openBlock, F as Fragment, h as renderList, l as createBlock } from "./index-BMEZeVej.js";
+import { _ as _export_sfc, c as createElementBlock, b as createBaseVNode, t as toDisplayString, j as withDirectives, n as normalizeClass, a as createVNode, w as withCtx, d as createTextVNode, i as createCommentVNode, k as resolveDirective, r as resolveComponent, u as useRouter, o as openBlock, F as Fragment, h as renderList, l as createBlock } from "./index-H_KRqZYt.js";
 const _sfc_main = {
   name: "Gallery-detail",
   props: {},
@@ -44,37 +44,52 @@ const _sfc_main = {
   }
 };
 const _hoisted_1 = { class: "gallery-detail" };
-const _hoisted_2 = { class: "d-flex align-items-center gallery_title" };
-const _hoisted_3 = { class: "flex-grow-1 text-center title_bar" };
-const _hoisted_4 = { class: "d-flex flex-column flex-lg-row mt-2 overflow-x-auto" };
-const _hoisted_5 = { class: "flex-grow-1 ms-3 d-flex flex-column overflow-hidden" };
-const _hoisted_6 = ["innerHTML"];
-const _hoisted_7 = { class: "info d-flex justify-content-between w-100; overflow-y-auto" };
-const _hoisted_8 = { class: "links" };
-const _hoisted_9 = { key: 0 };
-const _hoisted_10 = ["href"];
-const _hoisted_11 = ["href"];
-const _hoisted_12 = ["href"];
+const _hoisted_2 = { class: "mobile_gallery_title" };
+const _hoisted_3 = { class: "d-flex align-items-center gallery_title" };
+const _hoisted_4 = { class: "flex-grow-1 text-center title_bar" };
+const _hoisted_5 = { class: "d-flex align-items-center gallery_title" };
+const _hoisted_6 = { class: "flex-grow-1 text-center title_bar" };
+const _hoisted_7 = { class: "d-flex flex-column flex-lg-row mt-2 overflow-x-auto" };
+const _hoisted_8 = { class: "flex-grow-1 ms-3 d-flex flex-column overflow-hidden" };
+const _hoisted_9 = ["innerHTML"];
+const _hoisted_10 = { class: "info d-flex justify-content-between w-100; overflow-y-auto" };
+const _hoisted_11 = { class: "links" };
+const _hoisted_12 = { key: 0 };
+const _hoisted_13 = ["href"];
+const _hoisted_14 = ["href"];
+const _hoisted_15 = ["href"];
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   const _component_el_image = resolveComponent("el-image");
   const _component_el_carousel_item = resolveComponent("el-carousel-item");
   const _component_el_carousel = resolveComponent("el-carousel");
   const _directive_loading = resolveDirective("loading");
   return openBlock(), createElementBlock("div", _hoisted_1, [
-    withDirectives((openBlock(), createElementBlock("div", {
-      class: normalizeClass(["gallery-detail-main text-white", $data.info.vanilla == 1 ? "vanilla-item" : ""])
-    }, [
-      createBaseVNode("div", _hoisted_2, [
-        createBaseVNode("div", _hoisted_3, toDisplayString($data.info.title), 1),
-        _cache[3] || (_cache[3] = createBaseVNode("div", { class: "flex-grow-1 position-relative" }, null, -1)),
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("div", _hoisted_3, [
+        createBaseVNode("div", _hoisted_4, toDisplayString($data.info.title), 1),
+        _cache[4] || (_cache[4] = createBaseVNode("div", { class: "flex-grow-1 position-relative" }, null, -1)),
         createBaseVNode("div", {
           class: "close_btn",
           onClick: _cache[0] || (_cache[0] = ($event) => $setup.router.push("/gallery-list"))
-        }, _cache[2] || (_cache[2] = [
+        }, _cache[3] || (_cache[3] = [
+          createBaseVNode("span", { class: "material-symbols-outlined user-select-none" }, " close ", -1)
+        ]))
+      ])
+    ]),
+    withDirectives((openBlock(), createElementBlock("div", {
+      class: normalizeClass(["gallery-detail-main text-white", $data.info.vanilla == 1 ? "vanilla-item" : ""])
+    }, [
+      createBaseVNode("div", _hoisted_5, [
+        createBaseVNode("div", _hoisted_6, toDisplayString($data.info.title), 1),
+        _cache[6] || (_cache[6] = createBaseVNode("div", { class: "flex-grow-1 position-relative" }, null, -1)),
+        createBaseVNode("div", {
+          class: "close_btn",
+          onClick: _cache[1] || (_cache[1] = ($event) => $setup.router.push("/gallery-list"))
+        }, _cache[5] || (_cache[5] = [
           createBaseVNode("span", { class: "material-symbols-outlined user-select-none" }, " close ", -1)
         ]))
       ]),
-      createBaseVNode("div", _hoisted_4, [
+      createBaseVNode("div", _hoisted_7, [
         createVNode(_component_el_carousel, {
           class: "gallery-carousel flex-shrink-0",
           height: "100%",
@@ -102,49 +117,49 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
           ]),
           _: 1
         }),
-        createBaseVNode("div", _hoisted_5, [
+        createBaseVNode("div", _hoisted_8, [
           createBaseVNode("div", {
             class: "flex-grow-1 content_box",
             innerHTML: $data.info.content
-          }, null, 8, _hoisted_6),
-          createBaseVNode("div", _hoisted_7, [
+          }, null, 8, _hoisted_9),
+          createBaseVNode("div", _hoisted_10, [
             createBaseVNode("div", null, toDisplayString($data.info.date), 1),
             createBaseVNode("div", null, "Artist: " + toDisplayString($data.info.artist), 1)
           ]),
-          createBaseVNode("div", _hoisted_8, [
-            $data.info.link.rednote ? (openBlock(), createElementBlock("div", _hoisted_9, [
+          createBaseVNode("div", _hoisted_11, [
+            $data.info.link.rednote ? (openBlock(), createElementBlock("div", _hoisted_12, [
               createBaseVNode("a", {
                 class: "text-white",
                 href: $data.info.link.rednote
-              }, _cache[4] || (_cache[4] = [
+              }, _cache[7] || (_cache[7] = [
                 createBaseVNode("i", { class: "fa-solid fa-book" }, null, -1),
                 createTextVNode(" View in Rednote")
-              ]), 8, _hoisted_10)
+              ]), 8, _hoisted_13)
             ])) : createCommentVNode("", true),
             createBaseVNode("div", null, [
               $data.info.link.facebook ? (openBlock(), createElementBlock("a", {
                 key: 0,
                 class: "text-white",
                 href: $data.info.link.facebook
-              }, _cache[5] || (_cache[5] = [
+              }, _cache[8] || (_cache[8] = [
                 createBaseVNode("i", { class: "fa-brands fa-facebook" }, null, -1),
                 createTextVNode(" Artist Facebook")
-              ]), 8, _hoisted_11)) : createCommentVNode("", true)
+              ]), 8, _hoisted_14)) : createCommentVNode("", true)
             ]),
             createBaseVNode("div", null, [
               $data.info.link.forum ? (openBlock(), createElementBlock("a", {
                 key: 0,
                 class: "text-white",
                 href: $data.info.link.forum
-              }, _cache[6] || (_cache[6] = [
+              }, _cache[9] || (_cache[9] = [
                 createBaseVNode("i", { class: "fa-solid fa-paw" }, null, -1),
                 createTextVNode(" View in Forum")
-              ]), 8, _hoisted_12)) : createCommentVNode("", true)
+              ]), 8, _hoisted_15)) : createCommentVNode("", true)
             ])
           ])
         ])
       ]),
-      _cache[7] || (_cache[7] = createBaseVNode("div", { class: "text-center w-100 mt-2" }, [
+      _cache[10] || (_cache[10] = createBaseVNode("div", { class: "text-center w-100 mt-2" }, [
         createBaseVNode("strong", null, "The DNAxCAT character design is copyrighted to DNAxCAT")
       ], -1))
     ], 2)), [
@@ -152,11 +167,11 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     ]),
     createBaseVNode("div", {
       class: "gallery-detail-back",
-      onClick: _cache[1] || (_cache[1] = ($event) => $setup.router.push("/gallery-list"))
+      onClick: _cache[2] || (_cache[2] = ($event) => $setup.router.push("/gallery-list"))
     })
   ]);
 }
-const galleryDetail = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-d5f73ce7"]]);
+const galleryDetail = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-c7d13812"]]);
 export {
   galleryDetail as default
 };
--- assets/story-CYOGUIwk.js
+++ assets/story-CYOGUIwk.js
@@ -1,4 +1,4 @@
-import { _ as _export_sfc, c as createElementBlock, b as createBaseVNode, n as normalizeClass, t as toDisplayString, d as createTextVNode, a as createVNode, i as createCommentVNode, F as Fragment, h as renderList, r as resolveComponent, o as openBlock } from "./index-BMEZeVej.js";
+import { _ as _export_sfc, c as createElementBlock, b as createBaseVNode, n as normalizeClass, t as toDisplayString, d as createTextVNode, a as createVNode, i as createCommentVNode, F as Fragment, h as renderList, r as resolveComponent, o as openBlock } from "./index-H_KRqZYt.js";
 const _sfc_main = {
   name: "Story",
   props: {},
